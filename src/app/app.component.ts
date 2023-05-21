f4d57ab814785f7fc2afe5c78c9c5c60e15b6402
mahaut1 <mwindal@hotmail.com>
2023-05-21 10:52:43 +0200

    chapitre 2 et 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
-import { Component,OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'my-first-project';
-  constructor(){}
-  ngOnInit(): void{
-      console.log("Ici ngOnInit()")
+export class AppComponent {
+
+  message:string="";
+  onAffiche(arg: string) {
+    return this.message = "Merci d'avoir voté pour l'article : " + arg;
   }
+  liste = [
+    {
+      titreArticle: "Vélo",
+      prixArticle: 80,
+      description: "Un super vélo tout terrain",
+      urlImg: "../assets/images/velo.jpeg",
+      textAltImg: "Un vélo",
+      dispo: false
+    },
+    {
+      titreArticle: "TV",
+      prixArticle: 230,
+      description: "Très bonne qualité d'écran",
+      urlImg: "../assets/images/tv.jpeg",
+      textAltImg: "Une télévision",
+      dispo: true
+    },
+    {
+      titreArticle: "Jouet",
+      prixArticle: 15,
+      description: "Neuf, jamais utilisé",
+      urlImg: "../assets/images/jouet.jpeg",
+      textAltImg: "Un jouet pour enfant",
+      dispo: true
+    }
+  ]
 }
+
