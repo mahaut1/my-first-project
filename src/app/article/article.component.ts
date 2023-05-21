7c5c869a6d014f7f8bd966a6d137a65675ac668c
mahaut1 <mwindal@hotmail.com>
2023-05-21 10:51:31 +0200

    chapitre 2 et 3

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit {
+  
+  totalNbrLike: number=0;
+  comment: string = "Ceci est un commentaire";
+  @Output() info = new EventEmitter<string>();
 
+  @Input() titreArticle: string;
+  @Input() prixArticle: number;
+  @Input() description: string;
+  @Input() urlImg: string;
+  @Input() textAltImg: string;
+  @Input() dispo: boolean;
+
+  constructor(){}
+  ngOnInit(): void {
+      
+  }
+  onLike(){
+    this.totalNbrLike++;
+    this.info.emit(this.titreArticle);
+  }
+  getColor() {
+    if (this.dispo === true) {
+      return "green"
+    } else {
+      return "red"
+    }
+  }
 }
